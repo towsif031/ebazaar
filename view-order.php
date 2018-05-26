4b23f6ee532a369130ee98b05332663fe27b6875
Towsif Ahmed Omi <towsif031@gmail.com>
2018-05-26 20:41:47 +0600

    displayed customer details in view-order page

--- view-order.php
+++ view-order.php
@@ -111,29 +111,29 @@
 
                         <div class="row">
                             <div class="col-md-6">
-                                <h4>Billing Address <a href="#">Edit</a></h4>
-                                <p>
-                                    Mohamed Salah<br>
-                                    spyropress<br>
-                                    Dhaka<br>
-                                    Dhaka<br>
-                                    TR05<br>
-                                    342343<br>
-                                    Bangladesh
-                                </p>
+                                <h4>My Address <a href="#">Edit</a></h4>
+
+                                <?php
+                                    $csql = "SELECT u1.firstname, u1.lastname, u1.address1, u1.address2, u1.city, u1.state, u1.country, u1.company, u.email, u1.phone, u1.zip FROM users u JOIN usersmeta u1 WHERE u.id=u1.uid AND u.id=$uid";
+                                    $cres = mysqli_query($connection, $csql);
+                                    if(mysqli_num_rows($cres) == 1){
+                                        $cr = mysqli_fetch_assoc($cres);
+                                        echo "<p>". $cr['firstname'] ." ". $cr['lastname'] ."</p>";
+                                        echo "<p>". $cr['address1'] ."</p>";
+                                        echo "<p>". $cr['address2'] ."</p>";
+                                        echo "<p>". $cr['city'] ."</p>";
+                                        echo "<p>". $cr['state'] ."</p>";
+                                        echo "<p>". $cr['country'] ."</p>";
+                                        echo "<p>". $cr['company'] ."</p>";
+                                        echo "<p>". $cr['zip'] ."</p>";
+                                        echo "<p>". $cr['phone'] ."</p>";
+                                        echo "<p>". $cr['email'] ."</p>";
+                                    }
+								?>
                             </div>
 
                             <div class="col-md-6">
-                                <h4>Shipping Address <a href="#">Edit</a></h4>
-                                <p>
-                                    Mohamed Salah<br>
-                                    spyropress<br>
-                                    Dhaka<br>
-                                    Dhaka<br>
-                                    TR05<br>
-                                    342343<br>
-                                    Bangladesh
-                                </p>
+                                
                             </div>
                         </div>
                     </div>
