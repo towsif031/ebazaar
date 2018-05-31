0cd8c95e49df30c620084dc737bb750e3cdea8d9
Towsif Ahmed Omi <towsif031@gmail.com>
2018-05-31 19:07:52 +0600

    fixed login process

--- loginprocess.php
+++ loginprocess.php
@@ -9,15 +9,20 @@
 
         $sql = "SELECT * FROM users WHERE email='$email'";
         $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
+        $count = mysqli_num_rows($result);
         $r = mysqli_fetch_assoc($result);
 
-        if(password_verify($password, $r['password'])){
-            //echo "User exits! Create a seesion.";
-            $_SESSION['customer'] = $email;
-            $_SESSION['customerid'] = $r['id'];
-            header('location: checkout.php');
+        if($count == 1){
+            if(password_verify($password, $r['password'])){
+                //echo "User exits! Create a seesion.";
+                $_SESSION['customer'] = $email;
+                $_SESSION['customerid'] = $r['id'];
+                header('location: checkout.php');
+            }else{
+                // $fmsg = "Invalid login credentials!";
+                header('location: login.php?message=1');
+            }
         }else{
-            // $fmsg = "Invalid login credentials!";
             header('location: login.php?message=1');
         }
     }
